f0c5a06bac6bb0644f5e28a0767eebe1b42b2db7
Dennison Bertram <dennison@dennisonbertram.com>
2025-02-03 12:22:00 -0500

    fix: Switch to compiled JavaScript for tests

--- scripts/test-basic-operations.ts
+++ scripts/test-basic-operations.ts
@@ -1,12 +1,4 @@
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-import { createRequire } from 'module';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const require = createRequire(import.meta.url);
-
-const { PipeProtocol } = require(join(__dirname, '../src/pipe'));
+import { PipeProtocol } from '../src/pipe.js';
 
 async function testBasicOperations() {
   console.log('Starting basic operations test...\n');
